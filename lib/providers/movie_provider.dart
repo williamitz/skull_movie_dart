c309de789ae734e495967dd2eebeaf17040f0cbe
William Calle <william.usp@hotmail.com>
2022-04-29 18:19:28 -0500

    fix: optimization provider

--- lib/providers/movie_provider.dart
+++ lib/providers/movie_provider.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' show ChangeNotifier;
 import 'package:http/http.dart' as http;
 
 import 'package:skull_movie/models/models.dart';
@@ -13,6 +13,7 @@ class MovieProvider extends ChangeNotifier {
   final String _apiKey = 'e2a6f8ea1a51a3a6bb20c35a6b9ce7ab';
   final String _language = 'es-ES';
   int _page = 1;
+  int _popularPage = 1;
 
   List<Movie> _nowMovies = [];
   List<Movie> _popularMovies = [];
@@ -44,36 +45,46 @@ class MovieProvider extends ChangeNotifier {
     return _popularMovies.length;
   }
 
-  getNowMovies() async {
-    final url = Uri.https(_urlBase, '3/movie/now_playing',
-        {'api_key': _apiKey, 'page': _page.toString(), 'language': _language});
+  Future<String> _resolveHttp(String endpoint, int page) async {
+    final url = Uri.https(_urlBase, endpoint,
+        {'api_key': _apiKey, 'page': '$_page', 'language': _language});
 
     final response = await http.get(url);
+    return response.body;
+  }
 
-    if (response.statusCode == 200) {
-      _page++;
-      final MovieResponse movieResponse = MovieResponse.fromJson(response.body);
-      _nowMovies = [...movieResponse.results];
-    } else {
-      _nowMovies = [];
-    }
+  getNowMovies() async {
+    // final url = Uri.https(_urlBase, '3/movie/now_playing',
+    //     {'api_key': _apiKey, 'page': _page.toString(), 'language': _language});
+
+    // final response = await http.get(url);
+
+    final body = await _resolveHttp( '3/movie/now_playing', _page );
+
+    _page++;
+    final movieResponse = MovieResponse.fromJson(body);
+    _nowMovies = [...movieResponse.results];
 
     notifyListeners();
   }
 
   getPopularMovies() async {
-    final url = Uri.https(_urlBase, '3/movie/popular',
-        {'api_key': _apiKey, 'page': _page.toString(), 'language': _language});
+    // final url = Uri.https(_urlBase, '3/movie/popular',
+    //     {'api_key': _apiKey, 'page': _page.toString(), 'language': _language});
 
-    final response = await http.get(url);
+    // final response = await http.get(url);
 
-    if (response.statusCode == 200) {
-      _page++;
-      final PopularResponse popularResponse = PopularResponse.fromJson(response.body);
-      _popularMovies = [ ..._popularMovies, ...popularResponse.results];
-    } else {
-      _popularMovies = [];
-    }
+    final body = await _resolveHttp( '3/movie/popular', _popularPage );
+
+    _popularPage++;
+    final popularResponse = PopularResponse.fromJson(body);
+    
+    _popularMovies = [..._popularMovies, ...popularResponse.results];
+
+    // if (response.statusCode == 200) {
+    // } else {
+    //   _popularMovies = [];
+    // }
 
     notifyListeners();
   }
